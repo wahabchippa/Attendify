0860160acdc697ccc6cf971dbb3ec13852d2b6f9
wahabchippa <wahab.chippa@joinfleek.com>
2026-06-16 03:44:33 +0000

    android added

---
+++ android/app/src/main/java/com/chippa/attendify/MainActivity.java
@@ -0,0 +1,5 @@
+package com.chippa.attendify;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.chippa.attendify',
+  appName: 'Attendify',
+  webDir: 'dist'
+};
+
+export default config;
